4cf601645ff211f5d079c8343aa605fb4fbbfa17
Víctor Delgado Batista <63307570+Vic-28@users.noreply.github.com>
2025-03-01 13:31:13 +0100

    Create rush.c

---
+++ rush.c
@@ -0,0 +1,21 @@
+int main(char args, char **argsv)
+{
+    return(0);
+}
+
+
+
+
+int ft_boardGenerator()
+{
+    int size;
+    int subGridSize; //Para poder dividir los elementos del tablero en pequeños bloques
+
+    size = 4;
+    subGridSize = 2;
+
+
+
+}
+
+
